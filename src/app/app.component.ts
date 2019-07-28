a2ff7addc3ccdf54c330814c7dbac1c5f4569c5f
Rachid Annasabi <rachid.annasabi@exfo.com>
2019-07-28 10:06:23 -0400

    Add Header/Recipe/Shopping component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,4 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class AppComponent {
-  username = '';
-  showSecret = false;
-  log = [];
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    /* this.log.push(this.log.length + 1); */
-    this.log.push(new Date());
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,24 @@ import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
